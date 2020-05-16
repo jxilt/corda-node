e76c51bc0527a942054e8031346f1b3d0e7179ee
_ <_>
2020-05-16 13:33:23 +0100

    Parameterises the name of the generated node's folder.

--- create_node.sh
+++ create_node.sh
@@ -1,3 +1,9 @@
-mkdir node
-cp resources/node.conf node/node.conf
-curl -L https://r3.bintray.com/corda/net/corda/corda/4.4/corda-4.4.jar --output node/corda.jar
+if [ -z $1 ] 
+then
+    echo "The name of the node's folder must be passed as the first argument."
+    exit
+fi
+
+mkdir $1
+cp resources/node.conf $1/node.conf
+curl -L https://r3.bintray.com/corda/net/corda/corda/4.4/corda-4.4.jar --output $1/corda.jar
