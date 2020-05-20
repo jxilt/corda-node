a646c7d65ffbe3c2c6a9ee1fb4a0d28b8da90405
_ <_>
2020-05-20 18:36:15 +0100

    Adds an rpc operation for transferring an asset.

--- cordapp/clients/src/main/kotlin/jxilt/Client.kt
+++ cordapp/clients/src/main/kotlin/jxilt/Client.kt
@@ -1,20 +1,21 @@
 package jxilt
 
 import jxilt.flows.IssueAsset
+import jxilt.flows.TransferAsset
 import net.corda.client.rpc.CordaRPCClient
 import net.corda.core.messaging.CordaRPCOps
-import net.corda.core.messaging.RPCOps
 import net.corda.core.utilities.NetworkHostAndPort
 import net.corda.core.utilities.loggerFor
 import java.util.*
 
 /**
  * Connects to a Corda node via RPC and performs RPC operations on the node.
- *
- * The RPC connection is configured using command line arguments.
  */
 fun main(args: Array<String>) = Client(args).start()
 
+/**
+ * A generic client for interacting with a node running the asset CorDapp.
+ */
 private class Client(args: Array<String>) {
     companion object {
         val logger = loggerFor<Client>()
@@ -38,6 +39,7 @@ private class Client(args: Array<String>) {
     }
 
     // TODO: Provide ability to exit loop.
+    // Loops through the various possible operations and allows the user to select one.
     private fun selectAndExecuteOp() {
         println("Choose one of the following options:")
         clientOps.forEachIndexed { idx, op ->
@@ -57,33 +59,51 @@ private class Client(args: Array<String>) {
         chosenOp.execute(inputs)
     }
 
-    private val clientOps = listOf(IssueOp(rpcOps), TransferOp)
+    // The list of registered operations.
+    private val clientOps = listOf(IssueOp(rpcOps), TransferOp(rpcOps))
 }
 
+/**
+ * An abstract RPC client operation.
+ */
 private interface ClientOp {
+    val rpcOps: CordaRPCOps
     val opName: String
     val inputPrompts: List<String>
     fun execute(args: List<String>)
 }
 
-class IssueOp(val rpcOps: CordaRPCOps): ClientOp {
+/**
+ * An RPC client operation for issuing an asset.
+ */
+class IssueOp(override val rpcOps: CordaRPCOps): ClientOp {
     override val opName = "Issue asset."
     override val inputPrompts = listOf("Quantity to issue?")
 
     override fun execute(args: List<String>) {
-        require(args.size == 1) { "Incorrect number of args passed." }
+        require(args.size == inputPrompts.size) { "Incorrect number of args passed." }
         val quantity = args[0].toInt()
+
         rpcOps.startFlowDynamic(IssueAsset::class.java, quantity).returnValue.get()
         println("$quantity of the asset successfully issued.")
     }
 }
 
-object TransferOp: ClientOp {
+/**
+ * An RPC client operation for transferring an asset.
+ */
+class TransferOp(override val rpcOps: CordaRPCOps) : ClientOp {
     override val opName = "Transfer asset."
     override val inputPrompts = listOf("Quantity to transfer?", "New owner?")
 
     override fun execute(args: List<String>) {
-        // TODO: Update to perform logic.
-        println(args)
+        require(args.size == inputPrompts.size) { "Incorrect number of args passed." }
+        val quantity = args[0].toInt()
+        val matchingOwners = rpcOps.partiesFromName(args[1], true)
+        require(matchingOwners.size == 1) { "No single match found for new owner." }
+        val newOwner = matchingOwners.single()
+
+        rpcOps.startFlowDynamic(TransferAsset::class.java, quantity, newOwner).returnValue.get()
+        println("$quantity of the asset transferred to ${newOwner.name}.")
     }
 }
--- cordapp/clients/src/test/kotlin/jxilt/DriverBasedTest.kt
+++ cordapp/clients/src/test/kotlin/jxilt/DriverBasedTest.kt
@@ -7,13 +7,11 @@ import net.corda.testing.core.TestIdentity
 import net.corda.testing.core.singleIdentity
 import net.corda.testing.driver.DriverDSL
 import net.corda.testing.driver.DriverParameters
-import net.corda.testing.driver.NodeHandle
 import net.corda.testing.driver.driver
 import org.junit.Test
-import java.util.concurrent.Future
 import kotlin.test.assertEquals
 
-class DriverBasedTest {
+class ClientTests {
     private val bankA = TestIdentity(CordaX500Name("BankA", "", "GB"))
     private val bankB = TestIdentity(CordaX500Name("BankB", "", "US"))
 
@@ -30,6 +28,28 @@ class DriverBasedTest {
         val asset = assets.single().state.data
         assertEquals(quantity, asset.quantity)
         assertEquals(partyAHandle.nodeInfo.singleIdentity(), asset.owner)
+    }
+
+    @Test
+    fun `client can transfer assets`() = withDriver {
+        val (partyAHandle, partyBHandle) = startNodes(bankA, bankB)
+        val partyARpcOps = partyAHandle.rpc
+        val partyBRpcOps = partyBHandle.rpc
+
+        val quantity = 3
+        IssueOp(partyARpcOps).execute(listOf(quantity.toString()))
+
+        val newOwner = partyBHandle.nodeInfo.singleIdentity().name.organisation
+        TransferOp(partyARpcOps).execute(listOf(quantity.toString(), newOwner))
+
+        val assetsVaultA = partyARpcOps.vaultQuery(AssetState::class.java).states
+        assertEquals(0, assetsVaultA.size)
+
+        val assetsVaultB = partyBRpcOps.vaultQuery(AssetState::class.java).states
+        assertEquals(1, assetsVaultB.size)
+        val asset = assetsVaultB.single().state.data
+        assertEquals(quantity, asset.quantity)
+        assertEquals(partyBHandle.nodeInfo.singleIdentity(), asset.owner)
     }
 
     private fun withDriver(test: DriverDSL.() -> Unit) = driver(
