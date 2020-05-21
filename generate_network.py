3a43ffe53a28fa7b8ec6c1f7b5e672f5c1cf5238
_ <_>
2020-05-21 20:03:51 +0100

    Templates the creation of the node.conf files.

--- generate_network.py
+++ generate_network.py
@@ -1,6 +1,11 @@
 import os
+import pathlib
+from pathlib import Path
 import shutil
 import subprocess
+from string import Template
+
+BOOTSTRAPPER_URL = "https://software.r3.com/artifactory/corda-releases/net/corda/corda-tools-network-bootstrapper/4.4/corda-tools-network-bootstrapper-4.4.jar"
 
 class NodeInfo:
     def __init__(self, name, ip_address):
@@ -21,22 +26,24 @@ def generate_node_confs():
 
         nodes.append(NodeInfo(name, ip_address))
 
-    for node in nodes:
-        node_conf = """myLegalName = "O={name},L=New York,C=US"
-    p2pAddress = "{ip_address}:10000"
-    rpcSettings {{
-        address = "{ip_address}:10003"
-        adminAddress = "{ip_address}:10004"
-    }}""".format(name = node.name, ip_address = node.ip_address)
+    template_node_conf_file = open("../resources/template_node.conf", "r")
+    template_node_conf = Template(template_node_conf_file.read())
 
-        f = open(node.name + "_node.conf", "w")
-        try:
-            f.write(node_conf)
-        finally:
-            f.close()
+    try:
+        for node in nodes:
+            substitutions = {"name": node.name, "ip_address": node.ip_address}
+            node_conf = template_node_conf.safe_substitute(substitutions)
+
+            output_node_conf = open(node.name + "_node.conf", "w")
+            try:
+                output_node_conf.write(node_conf)
+            finally:
+                output_node_conf.close()
+    finally:
+        template_node_conf_file.close()
 
 def download_and_run_bootstrapper():
-    bootstrapper_url = "https://software.r3.com/artifactory/corda-releases/net/corda/corda-tools-network-bootstrapper/4.4/corda-tools-network-bootstrapper-4.4.jar"
+    bootstrapper_url = BOOTSTRAPPER_URL
     bootstrapper_filename = "bootstrapper.jar"
 
     subprocess.run(["curl", "-L", bootstrapper_url, "--output", bootstrapper_filename])
@@ -44,9 +51,8 @@ def download_and_run_bootstrapper():
 
 def clean_up():
     shutil.rmtree(".cache")
-    os.remove("checkpoints_agent-*.log")
-    os.remove("diagnostic-*.log")
-    os.remove("node-*.log")
+    for file in Path(".").glob("*.log"):
+        pathlib.remove(file)
 
 os.mkdir("generated_network")
 os.chdir("generated_network")
